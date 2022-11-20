2d6922092f097c35028e80407ab4edbbcec18d94
ostromann <oliver.stromann@gmail.com>
2022-11-20 16:00:19 +0100

    Prepare for export.

--- GLOBAL.gd
+++ GLOBAL.gd
@@ -2,11 +2,19 @@ extends Node
 
 var level_list = [
 	"res://Level/Level1.tscn",
-#	"res://Level/Level2.tscn",
-#	"res://Level/Level3.tscn", 
+	"res://Level/Level2.tscn",
+	"res://Level/Level3.tscn", 
+	"res://Level/Level4.tscn", 
+	"res://Level/Level5.tscn", 
+	"res://Level/Level6.tscn", 
+	"res://Level/Level7.tscn", 
+	"res://Level/Level8.tscn", 
+	"res://Level/Level9.tscn", 
+	"res://Level/Level10.tscn", 
 	]
 var level_index : int = 0
 var is_playing : bool = false
+#var shows_winning_screen : bool = false
 
 const START_MENU = preload("res://Level/MainMenuBackdrop.tscn")
 const END_MENU = preload("res://Level/EndMenuBackdrop.tscn")
@@ -14,12 +22,15 @@ const END_MENU = preload("res://Level/EndMenuBackdrop.tscn")
 onready var current_level = $MainMenuBackdrop
 
 func _ready():
+	$AudioStreamPlayer.play()
 	pass
 #	add_to_group("GLOBAL")
 
 func _process(delta):
-	if Input.is_action_just_pressed("ui_start"):
-		get_tree().change_scene("res://UI/MainMenu.tscn")
+	pass
+
+#	if Input.is_action_just_pressed("ui_start"):
+#		get_tree().change_scene("res://UI/MainMenu.tscn")
 
 	
 func next_level():
@@ -37,6 +48,8 @@ func next_level():
 		load_end_screen()
 		
 func load_start_screen():
+	$VictoryAudioStreamPlayer.stop()
+	$AudioStreamPlayer.play()
 	var next_level = START_MENU.instance()
 	current_level.queue_free()
 	add_child(next_level)
@@ -45,11 +58,13 @@ func load_start_screen():
 	$StartScreen.show()
 
 func load_end_screen():
+	$AudioStreamPlayer.stop()
+	$VictoryAudioStreamPlayer.play()
+	$ShowEndScreenTimer.start()
 	var next_level = END_MENU.instance()
 	current_level.queue_free()
 	add_child(next_level)
 	current_level = next_level
-	$EndScreen.show()
 	$StartScreen.hide()
 	
 
@@ -63,4 +78,6 @@ func _on_RestartButton_pressed():
 	level_index = 0
 	$EndScreen.hide()
 	load_start_screen()
-	
+
+func _on_ShowEndScreenTimer_timeout():
+	$EndScreen.show()
